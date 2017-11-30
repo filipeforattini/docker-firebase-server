b15c13d67f5d83bd9776bf8718802d47c45cc36e
Filipe Forattini <filipeforattini1@gmail.com>
2017-11-29 22:20:42 -0200

    First commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 VERSION=$(git rev-parse --abbrev-ref HEAD)
-echo "  VERSION: ${VERSION}"
 docker build --rm -t filipeforattini/firebase-server:$VERSION .
 docker push filipeforattini/firebase-server:$VERSION
